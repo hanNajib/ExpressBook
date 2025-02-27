de53ea0a846696ebeb068c37dcdfa33d005b89de
hanNajib <arroyhan27@gmail.com>
2025-02-27 09:14:25 +0700

    Setup Express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config();
+
+
+const port = process.env.PORT
+const app = express();
+
+app.listen(port, () => {
+    console.info("Server Started at port " + port)
+})
